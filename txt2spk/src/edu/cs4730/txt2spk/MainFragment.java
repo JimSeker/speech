bbac359fe541859d07071d539a75417365da120b
JimSeker <seker@uwyo.edu>
2014-07-30 10:33:55 -0600

    updated to 4.4.4 added fragment.

--- txt2spk/src/edu/cs4730/txt2spk/MainFragment.java
+++ txt2spk/src/edu/cs4730/txt2spk/MainFragment.java
@@ -1,60 +1,68 @@
 package edu.cs4730.txt2spk;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class txt2spk extends Activity implements OnInitListener {
-  private EditText words = null;
-  private Button speakBtn = null;
-  private static final int REQ_TTS_STATUS_CHECK = 0;
-  private static final String TAG = "TTS Demo";
-  private TextToSpeech mTts;
+/**
+ * A simple {@link Fragment} subclass.
+ * 
+ */
+public class MainFragment extends Fragment implements OnInitListener {
+	  private EditText words = null;
+	  private Button speakBtn = null;
+	  private static final int REQ_TTS_STATUS_CHECK = 0;
+	  private static final String TAG = "TTS Demo";
+	  private TextToSpeech mTts;
 
-  
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        words = (EditText)findViewById(R.id.wordsToSpeak);
-        speakBtn = (Button)findViewById(R.id.speak);
+	
+	
+	public MainFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View myView = inflater.inflate(R.layout.main_fragment, container, false);
+		
+        words = (EditText) myView.findViewById(R.id.wordsToSpeak);
+        speakBtn = (Button) myView.findViewById(R.id.speak);
         speakBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+            	//Speech is simple.  send the words to speech aloud via the 
+            	//the text to speech end and add it to the end queue. (maybe others already in line.)
                 mTts.speak(words.getText().toString(), TextToSpeech.QUEUE_ADD, null);
             }});
 
         // Check to be sure that TTS exists and is okay to use
         Intent checkIntent = new Intent();
         checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        //The result will come back in onActivityResult with our REQ_TTS_STATUS_CHECK number
         startActivityForResult(checkIntent, REQ_TTS_STATUS_CHECK);
-    }
-    
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+        return myView;
+	}
+
+	
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQ_TTS_STATUS_CHECK) {
             switch (resultCode) {
             case TextToSpeech.Engine.CHECK_VOICE_DATA_PASS:
                 // TTS is up and running
-                mTts = new TextToSpeech(this, this);
+                mTts = new TextToSpeech(getActivity(), this);
                 Log.v(TAG, "Pico is installed okay");
-                break;
-            case TextToSpeech.Engine.CHECK_VOICE_DATA_BAD_DATA:
-            case TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_DATA:
-            case TextToSpeech.Engine.CHECK_VOICE_DATA_MISSING_VOLUME:
-                // missing data, install it
-                Log.v(TAG, "Need language stuff: " + resultCode);
-                Intent installIntent = new Intent();
-                installIntent.setAction(
-                    TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-                startActivity(installIntent);
                 break;
             case TextToSpeech.Engine.CHECK_VOICE_DATA_FAIL:
             default:
@@ -86,6 +94,7 @@ public class txt2spk extends Activity implements OnInitListener {
     @Override
     public void onDestroy()
     {
+    	//make sure and shutdown the text to speech engine.
         super.onDestroy();
         mTts.shutdown();
     }
