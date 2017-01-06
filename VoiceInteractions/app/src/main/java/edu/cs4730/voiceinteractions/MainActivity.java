596dc73f2a883e70e9c7b72add79d611f391accf
JimSeker <seker@uwyo.edu>
2017-01-05 19:03:13 -0700

    fixed the example
    Removed a lot of the play music demo, which was never going to work I
    suspect.  changed to taking pictures.

--- VoiceInteractions/app/src/main/java/edu/cs4730/voiceinteractions/MainActivity.java
+++ VoiceInteractions/app/src/main/java/edu/cs4730/voiceinteractions/MainActivity.java
@@ -1,21 +1,46 @@
 package edu.cs4730.voiceinteractions;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /*
  * Nothing really to see here, it is all done in VoiceInteractionActivity
  */
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        finish();
 
+//        finish();
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        if (intent == null) {
+            Log.v(TAG, "No Intent!");
+        } else if (isVoiceInteraction()) {
+            Log.v(TAG, "Intent is " + intent.getAction());
+            Log.v(TAG, "it's working!");
+        } else if (isVoiceInteractionRoot()) {  //started by voice only?  I think. doc's are not clear here.
+            Log.v(TAG, "Intent is " + intent.getAction());
+            Log.v(TAG, "it's working?");
+        } else {
+            Log.v(TAG, "Intent is " + intent.getAction());
+            Log.v(TAG, "No Voice Interaction");
+
+        }
+       // finish();
     }
 }
--- VoiceInteractions/app/src/main/java/edu/cs4730/voiceinteractions/VoiceInteractionActivity.java
+++ VoiceInteractions/app/src/main/java/edu/cs4730/voiceinteractions/VoiceInteractionActivity.java
@@ -2,145 +2,191 @@ package edu.cs4730.voiceinteractions;
 
 import android.app.VoiceInteractor;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.android.gms.actions.NoteIntents;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
-//https://developers.google.com/voice-actions/interaction/
-//http://io2015codelabs.appspot.com/codelabs/voice-interaction#6
 
-/*
- * This example doesn't work very well.  It attempts to recreate the google example of tuneIn
- *   Speak: Play music on tuneIn   , but I don't get any voiceinteraction
+/*   Voice interactions are problematic and my guess are going to be replaced with the google actions anyway.
+ *   many of the intents don't support voice (even though you can take it anyway, like all the music intents left in this example)
  *
- *   It also has the image intent.  So say Take a selfie  and it will show that voiceinteraction does work.
+ *   this example show the command "Take a selfie" or  "take a picture."
+ *   speak one of the two commands.  selfie impies the front camera, so it will only confirm you action
+ *   while take a picture will ask front or back camera.
+ *
+ *   btw, te taxi example they show on the main page, only works from android wear....
+ *   https://developers.google.com/voice-actions/interaction/voice-interactions
+ *
+ *   The base of this code is from here http://io2015codelabs.appspot.com/codelabs/voice-interaction#6  but has been heavy cleaned up and changed.
  */
 
 public class VoiceInteractionActivity extends AppCompatActivity {
     ListView myList;
-    String[] values = new String[]{"Blues", "Classical", "Country", "Folk", "Jazz", "Pop", "Rock",
-            "Rap", "Heavy Metal"};
+    String[] values = new String[]{"Front", "Back", "Other"};
     String TAG = "VoiceActionAct";
-   // private static final Uri APP_URI = Uri.parse("android-app://edu.cs4730.voiceinteractions/VoiceInteractionActivity");
-
-    //private GoogleApiClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voiceinteraction);
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
         Intent intent = getIntent();
-        if (intent == null) {
+        if (intent == null) {  //not sure how this would happen, so just exit.
             Log.v("TAG", "No Intent!");
             finish();
 
-            // } else if (intent.getAction().equals("com.google.android.gms.actions.CREATE_NOTE")) {  //direct action intent name.
         } else if (isVoiceInteraction()) {
             Log.v(TAG, "Intent is " + intent.getAction());
-        //What the hell are the keys and all the data anyway?  There are a lot of them.
+            printkeysAnd(intent);
+
+        } else if (isVoiceInteractionRoot()) {  //started by voice only?  I think. doc's are not clear here.
+            Log.v(TAG, "Intent is " + intent.getAction());
+            printkeysAnd(intent);
+            Log.v(TAG, "it's working!?");
+        } else {
+            Log.v(TAG, "Intent is " + intent.getAction());
+            //print the keys anyway, to see that is there.
+            Bundle ab = intent.getExtras();
+            for (String key : ab.keySet()) {
+                Object value = ab.get(key);
+                Log.d("VAA Key", String.format("%s %s (%s)", key,
+                        value.toString(), value.getClass().getName()));
+            }
+            Log.v(TAG, "No Voice Interaction");
+            finish();
+        }
+    }
+
+    /*
+     *  a helper function to print out all the keys in the intent check if android.intent.extra.USE_FRONT_CAMERA is true.
+     */
+    private void printkeysAnd(Intent intent) {
+        boolean frontcamera = false;
+        //first print out are the keys and all the data anyway?  There are a lot of them.
         Bundle ab = intent.getExtras();
+        frontcamera = ab.getBoolean("android.intent.extra.USE_FRONT_CAMERA", false);
         for (String key : ab.keySet()) {
             Object value = ab.get(key);
-            Log.d(TAG, String.format("%s %s (%s)", key,
+            Log.d("VAA Key", String.format("%s %s (%s)", key,
                     value.toString(), value.getClass().getName()));
         }
+        Log.v(TAG, "It worked, onto Next step.");
 
-            Log.v(TAG, "It worked, onto Next step.");
-
-            //setup the display
+        if (frontcamera) { //said take a selfie, so default to front camera, just confirm.
+            //so now confirm it, then fake the picture and say success I guess.
+            String ttsPrompt = "Use the front camera";
+            String visualPrompt = "Using the front camera?";
+            getVoiceInteractor().submitRequest(new Confirm(ttsPrompt, visualPrompt));
+        } else {
+            //draw the list so the user can see what the options are, using a simple listview.
             myList = (ListView) findViewById(R.id.list1);
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
                     android.R.layout.simple_list_item_1, values);
             myList.setAdapter(adapter);
             //now setup and call the initial voice trigger.
             startVoiceTrigger();
-
-        } else {
-            Log.v(TAG, "Intent is " + intent.getAction());
-            Log.v(TAG, "No Voice Interaction");
-            finish();
         }
-
-
     }
 
+    /*
+     * extend the request class, so the the work for Take a selfie.
+     */
+    class Confirm extends VoiceInteractor.ConfirmationRequest {
+        public Confirm(String ttsPrompt, String visualPrompt) {
+            //super must come first, so their code is well wrong... shocker...
+            super(new VoiceInteractor.Prompt(
+                            new String[]{ttsPrompt}, visualPrompt
+                    )
+                    , null);
+        }
+
+        @Override
+        public void onConfirmationResult(boolean confirmed, Bundle result) {
+            Bundle status = new Bundle();  //the picture should be in the bundle.
+            VoiceInteractor.Request request = null;
+
+            if (confirmed) {
+                //here is where we would take the picture.  except I'm faking it.
+                request = new VoiceInteractor.CompleteVoiceRequest(new VoiceInteractor.Prompt("Success"), status);
+            } else {
+                request = new VoiceInteractor.AbortVoiceRequest(new VoiceInteractor.Prompt("Too Complex"), status);
+            }
+            getVoiceInteractor().submitRequest(request);
+
+            finish();
+        }
+    }
+
+    /*
+     * instead of extending the class, we are just using the submitRequest and options in the method.
+     */
     private void startVoiceTrigger() {
+
         Log.d(TAG, "startVoiceTrigger: ");
-        if (isVoiceInteraction()) {
-            String[] values = new String[]{"Blues", "Classical", "Country", "Folk", "Jazz", "Pop", "Rock",
-                    "Rap", "Heavy Metal"};
 
-            //Doing this the Hard way likely.
-            VoiceInteractor.PickOptionRequest.Option[] mOptions = new VoiceInteractor.PickOptionRequest.Option[]{
-                    new VoiceInteractor.PickOptionRequest.Option(values[0], 0),
-                    new VoiceInteractor.PickOptionRequest.Option(values[1], 1),
-                    new VoiceInteractor.PickOptionRequest.Option(values[2], 2),
-                    new VoiceInteractor.PickOptionRequest.Option(values[3], 3),
-                    new VoiceInteractor.PickOptionRequest.Option(values[4], 4),
-                    new VoiceInteractor.PickOptionRequest.Option(values[5], 5),
-                    new VoiceInteractor.PickOptionRequest.Option(values[6], 6),
-                    new VoiceInteractor.PickOptionRequest.Option(values[7], 7),
-                    new VoiceInteractor.PickOptionRequest.Option(values[8], 8).addSynonym("Angry White boy Music")
-            };
-//            VoiceInteractor.PickOptionRequest.Option option1 = new VoiceInteractor.PickOptionRequest.Option("work", 1);
-//            option1.addSynonym("business");
+        //Doing this the Hard way likely.
+        VoiceInteractor.PickOptionRequest.Option[] mOptions = new VoiceInteractor.PickOptionRequest.Option[]{
+                new VoiceInteractor.PickOptionRequest.Option(values[0], 0),
+                new VoiceInteractor.PickOptionRequest.Option(values[1], 1).addSynonym("default"),
+                new VoiceInteractor.PickOptionRequest.Option(values[2], 2)
+        };
 
-            VoiceInteractor.Prompt prompt = new VoiceInteractor.Prompt("Pick a Category");
-            getVoiceInteractor().submitRequest(new VoiceInteractor.PickOptionRequest(prompt, mOptions, null) {
-                @Override
-                public void onPickOptionResult(boolean finished, Option[] selections, Bundle result) {
-                    if (finished && selections.length >= 1) {
-                        //so doc's say it could be more then one selection, so just choose first one.
-                        Log.v(TAG, "Option is " + selections[0].getLabel());
-                        SendComplete(selections[0].getLabel().toString());
-                    } else {
-                        Log.v(TAG, "No Selection?");
-                        finish();
-                    }
-                }
+        //instead of extending the class, just use it here.
+        VoiceInteractor.Prompt prompt = new VoiceInteractor.Prompt("Pick which camera");
+        getVoiceInteractor().submitRequest(new VoiceInteractor.PickOptionRequest(prompt, mOptions, null) {
+            @Override
+            public void onPickOptionResult(boolean finished, Option[] selections, Bundle result) {
+                if (finished && selections.length >= 1) {
+                    //so doc's say it could be more then one selection, so just choose first one.
+                    Log.v(TAG, "Option is " + selections[0].getLabel());
 
-                @Override
-                public void onCancel() {
-                    Log.v(TAG, "User Canceled.");
+                    //here we would use the camera
+
+                    //then send the complete
+                    getVoiceInteractor().submitRequest(
+                            new VoiceInteractor.CompleteVoiceRequest(
+                                    new VoiceInteractor.Prompt("Took picture with " + selections[0].getLabel()),
+                                    new Bundle()
+                            ) {  //anonymous class to see the complete result message.  Which should be uninteresting.
+                                @Override
+                                public void onCompleteResult(Bundle result) {
+                                    super.onCompleteResult(result);
+                                    Log.d(TAG, "OnCompleteResult:");  //dig out the result in bundle if we really cared.
+                                }
+                            }
+                    );
                     finish();
-
+                } else {  //nothing was selected, so...
+                    getVoiceInteractor().submitRequest(
+                            new VoiceInteractor.AbortVoiceRequest(
+                                    new VoiceInteractor.Prompt("Too Complex"),
+                                    new Bundle()
+                            )
+                    );
+                    finish();
                 }
-            });
-        } else {
-            Log.v(TAG, "IsVoiceInterACtion is false Now?");
-            finish();
-        }
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v(TAG, "User Canceled.");
+                getVoiceInteractor().submitRequest(
+                        new VoiceInteractor.AbortVoiceRequest(
+                                new VoiceInteractor.Prompt("User Canceled"),
+                                new Bundle()
+                        )
+                );
+                finish();
+
+            }
+        });
     }
-
-
-    public void SendComplete(String genre) {
-        //
-        VoiceInteractor.Prompt prompt = new VoiceInteractor.Prompt("Playing "+ genre);
-        Bundle status = new Bundle();
-        getVoiceInteractor().submitRequest(
-                new VoiceInteractor.CompleteVoiceRequest(prompt, status) {
-                    @Override
-                    public void onCompleteResult(Bundle result) {
-                        super.onCompleteResult(result);
-                        Log.d(TAG, "OnCompleteResult:" + Log.getStackTraceString(new Exception()));
-                    }
-                });
-
-
-        finish();
-    }
-
-
 }
